de8c443b035370da6cdff075c436b0b58d52533c
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-04-04 18:42:34 +0200

    fix: improve path

--- cdk/http-api-mock.ts
+++ cdk/http-api-mock.ts
@@ -2,6 +2,8 @@ import { packLayer } from '@bifravst/aws-cdk-lambda-helpers/layer'
 import { packLambdaFromPath } from '@bifravst/aws-cdk-lambda-helpers'
 import { HTTPAPIMockApp } from './App.js'
 import { fromEnv } from '@nordicsemiconductor/from-env'
+import path, { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
 const { stackName } = fromEnv({ stackName: 'STACK_NAME' })(process.env)
 
@@ -10,6 +12,7 @@ new HTTPAPIMockApp(stackName, {
 		httpApiMock: await packLambdaFromPath(
 			'httpApiMock',
 			'cdk/resources/http-api-mock-lambda.ts',
+			path.join(dirname(fileURLToPath(import.meta.url)), '..', '..'),
 		),
 	},
 	layer: await packLayer({
